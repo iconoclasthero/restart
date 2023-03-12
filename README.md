bcc3952be3306503bcaf876812ecd7b65156e67b
bjv <iconoclasthero@gmail.com>
2023-03-12 00:18:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+In order to get the completion script to work, it needs to be sourced prior to running the restart script.
+The way I do it, I add the completion script to ~/.bashrc, but if you wanted to avoid this overhead, it could be sourced prior to calling the restart command via an alias. 
+
---
+++ restart-completion.bash
@@ -0,0 +1,27 @@
+#have restart &&
+_restart_complete()
+{
+  local cur prev
+
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  if [ $COMP_CWORD -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "alsa bind bind9 deluge deluged icecast mpd network networking nfs now pulse pulseaudio serviio shout minidlna" -- $cur) )
+  elif [ $COMP_CWORD -eq 2 ]; then
+    case "$prev" in
+      "minidlna")
+        COMPREPLY=( $(compgen -W "restart force-reload rescan" -- $cur) )
+        ;;
+#      "deploy")
+#        COMPREPLY=( $(compgen -W "all current" -- $cur) )
+#        ;;
+      *)
+        ;;
+    esac
+  fi
+
+  return 0
+} &&
+complete -F _restart_complete restart
